1d33e31ccea441a0488f1cf80f4bfe3a031f8529
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2024-02-28 09:54:36 +0100

    Add project files.

---
+++ Inlämningsuppgift 6.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_6._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addera(1, 2, 3));
+
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            return summa;
+        }
+    }
+}
